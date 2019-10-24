633c46bf7402e44740b0ab971063a83afb04e0f0
Brian Cassidy <brian.cassidy@unb.ca>
2019-10-24 10:57:27 -0300

    set https url for non-local deploy

--- scripts/pre-init.d/58_update_config.sh
+++ scripts/pre-init.d/58_update_config.sh
@@ -3,3 +3,8 @@ sed -i "s|MYSQL_HOSTNAME|$MYSQL_HOSTNAME|g" /app/html/application/config/databas
 sed -i "s|MYSQL_DATABASE|$MYSQL_DATABASE|g" /app/html/application/config/database.php
 sed -i "s|MYSQL_USERNAME|$MYSQL_USERNAME|g" /app/html/application/config/database.php
 sed -i "s|MYSQL_PASSWORD|$MYSQL_PASSWORD|g" /app/html/application/config/database.php
+
+if [ "$DEPLOY_ENV" != "local" ]; then
+  echo "Setting HTTPS for non-local deployment..."
+  echo "\$config['base_url'] = str_replace('http:', 'https:', \$config['base_url']);" >> /app/html/application/config/config.php
+fi
