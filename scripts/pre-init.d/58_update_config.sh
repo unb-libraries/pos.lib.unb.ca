1b5f431258de0a035e812c1d16df656bcefb4dd7
Brian Cassidy <brian.cassidy@unb.ca>
2019-12-04 15:43:58 -0400

    Set 24-hour-long session expiry

--- scripts/pre-init.d/58_update_config.sh
+++ scripts/pre-init.d/58_update_config.sh
@@ -4,6 +4,9 @@ sed -i "s|MYSQL_DATABASE|$MYSQL_DATABASE|g" /app/html/application/config/databas
 sed -i "s|MYSQL_USERNAME|$MYSQL_USERNAME|g" /app/html/application/config/database.php
 sed -i "s|MYSQL_PASSWORD|$MYSQL_PASSWORD|g" /app/html/application/config/database.php
 
+# Set 24-hour-long session expiry
+echo "\$config['sess_expiration'] = 86400;" >> /app/html/application/config/config.php
+
 if [ "$DEPLOY_ENV" != "local" ]; then
   echo "Setting HTTPS for non-local deployment..."
   echo "\$config['base_url'] = str_replace('http:', 'https:', \$config['base_url']);" >> /app/html/application/config/config.php
